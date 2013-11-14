7a36c20cef5bf7a5e2960ec6dd8d60ca56b03adc
moneil113 <Matthew@pcp077817pcs.wireless.calpoly.edu>
2013-11-14 11:43:02 -0800

    Exit Segues

---
+++ EZ Dining/EZ Dining/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  EZ Dining
+//
+//  Created by Matthew O'Neil on 11/14/13.
+//  Copyright (c) 2013 Matthew O'Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
